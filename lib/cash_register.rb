6682372351ed919c7d0d7a232e47a63186413eb9
JBondeson19 <jpbond58@gmail.com>
2020-08-26 18:11:49 -0400

    working lab

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,26 @@
+class CashRegister
+
+    attr_accessor :discount, :total, :title, :price
+    def initialize(total= 0, discount= 20)
+        @total = total
+        @discount = discount
+    end
+    
+    def current_total
+        self.total = int
+    end 
+
+
+    def add_item
+        hash = {:title => " ", :price = int }
+        #name for the item ("title")
+        "dkdkd #{self.title} and #{self.total.}"
+        #price for said item ("price = 1.00 or 0.90 etc")
+        #add price to the current total.
+        current_total << hash 
+
+    end
+
+end
+
 
